9d6328c08f5dac3fd83dbab53e8c2155f4776a63
hugo <hugo.prd.br@gmail.com>
2025-07-02 21:31:48 -0300

    criacao metodo insere teste

--- src/OrdenacaoTopologica.java
+++ src/OrdenacaoTopologica.java
@@ -43,6 +43,34 @@ public class OrdenacaoTopologica{
 		prim = null;
 		n = 0;
 	}
+
+	public Elo insere(int chave){
+		if(prim == null){
+            prim = new Elo();
+            n++;
+
+            return prim;
+        }
+
+        Elo p = prim;
+        while(p.prox != null){
+            if(p.chave == chave){
+                return p;
+            }
+			
+            p = p.prox;
+        }
+
+        if(p.chave == chave){
+            return p;
+        }
+
+        // Se não encontrou, cria um novo no final da lista
+        Elo novoElo = new Elo(chave);
+        p.prox = novoElo;
+        n++;
+        return novoElo;
+	}
 	
 	/* Método responsável pela leitura do arquivo de entrada. */
 	public void realizaLeitura(String nomeEntrada){
