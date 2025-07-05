bdddfeebd696024e53264fa1e554cc3811f89c07
hugo <hugo.prd.br@gmail.com>
2025-07-05 13:46:33 -0300

    implementacao realizaLeitura

--- src/OrdenacaoTopologica.java
+++ src/OrdenacaoTopologica.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class OrdenacaoTopologica{
 	private class Elo{
 		public int chave;
@@ -76,7 +80,38 @@ public class OrdenacaoTopologica{
 	
 	/* Método responsável pela leitura do arquivo de entrada. */
 	public void realizaLeitura(String nomeEntrada){
-		/* Preencher. */
+		File arquivo = new File(nomeEntrada);
+
+		try(Scanner scan = new Scanner(arquivo)){
+			while(scan.hasNextInt()){
+				int chaveAnt = scan.nextInt();
+
+				if(scan.hasNext("<")){
+					scan.next();
+				}
+
+				int chaveSuc = scan.nextInt();
+
+				Elo noAnt = buscaElo(chaveAnt);
+				if(noAnt == null){
+					noAnt = criaElo(chaveAnt);
+				}
+
+				Elo noSuc = buscaElo(chaveSuc);
+				if(noSuc == null){
+					noSuc = criaElo(chaveSuc);
+				}
+
+				EloSuc novoSuc = new EloSuc(noSuc, noAnt.listaSuc);
+				noAnt.listaSuc = novoSuc;
+
+				noSuc.contador++;
+			}
+		}
+		catch(FileNotFoundException e){
+			System.err.println("Erro: Arquivo '" + nomeEntrada + "' não encontrado.");
+            e.printStackTrace();
+		}
 	}
 	
 	/* Método para impressão do estado atual da estrutura de dados. */
